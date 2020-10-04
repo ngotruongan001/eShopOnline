ba457236f2c7860b4e0a4c34d316d90a027fb729
Ngo Truong An <anngo270398@gmail.com>
2020-10-04 11:03:45 +0700

    Initial solution and repository

---
+++ eShopOnline.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShopOnline.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
